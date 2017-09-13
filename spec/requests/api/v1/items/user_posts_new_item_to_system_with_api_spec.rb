cd707f67963801ce17c94fe0953c569ebda7794c
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-13 09:53:56 -0600

    more robust test passes with json response

--- spec/requests/api/v1/items/user_posts_new_item_to_system_with_api_spec.rb
+++ spec/requests/api/v1/items/user_posts_new_item_to_system_with_api_spec.rb
@@ -11,8 +11,11 @@ describe 'items api' do
     post '/api/v1/items', item
 
     result = JSON.parse(response.body)
-
     expect(status).to be(201)
     expect(Item.count).to eq(1)
+    expect(result["body"]["id"]).to eq(Item.first.id)
+    expect(result["body"]["name"]).to eq(Item.first.name)
+    expect(result["body"]["description"]).to eq(Item.first.description)
+    expect(result["body"]["image_url"]).to eq(Item.first.image_url)
   end
 end
