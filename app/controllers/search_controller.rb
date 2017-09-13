757e19f1b9f3bc9a7629fdbbfd1297af4933cff8
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-13 10:44:22 -0600

    added best buy api key to application.yml, confirmed request still pulls correctly

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 
   def index
     @conn = Faraday.new(url: "https://api.bestbuy.com")
-    response = @conn.get("/v1/stores(area(80202,25))?format=json&show=storeType,longName,city,phone,distance&apiKey=zvkqfuc5vws6ytvedk2qwf4n")
+    response = @conn.get("/v1/stores(area(80202,25))?format=json&show=storeType,longName,city,phone,distance&apiKey=#{ENV['bb_key']}")
     result = JSON.parse(response.body)
     binding.pry
   end
