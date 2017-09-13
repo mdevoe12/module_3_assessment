5f9eec566bda4b1ea267c90283ce6782dc41fe62
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-13 10:48:07 -0600

    created Store.rb PORO

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,9 +2,9 @@ class SearchController < ApplicationController
 
   def index
     @conn = Faraday.new(url: "https://api.bestbuy.com")
-    response = @conn.get("/v1/stores(area(80202,25))?format=json&show=storeType,longName,city,phone,distance&apiKey=#{ENV['bb_key']}")
+    response = @conn.get("/v1/stores(area(#{params[:search]},25))?format=json&show=storeType,longName,city,phone,distance&apiKey=#{ENV['bb_key']}")
     result = JSON.parse(response.body)
-    binding.pry
+    @stores = Store.find_stores(params[:search])
   end
 
 end
---
+++ app/models/store.rb
@@ -0,0 +1,6 @@
+class Store
+
+
+
+
+end
