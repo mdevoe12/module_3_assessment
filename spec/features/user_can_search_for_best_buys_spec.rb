d8fe939edcfef343b9384c35d2a829b9954738ef
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-13 11:22:17 -0600

    added test for expect to find 10 .store classes

--- spec/features/user_can_search_for_best_buys_spec.rb
+++ spec/features/user_can_search_for_best_buys_spec.rb
@@ -20,11 +20,7 @@ RSpec.feature "a user can see a list of best buy stores" do
         expect(page).to have_content("303-270-9189")
         expect(page).to have_content("Mobile SAS")
       end
+      expect(all(".store").count).to eq(10)
     end
-# Then my current path should be "/search" (ignoring params)
-# And I should see stores within 25 miles of 80202
-# And I should see a message that says "17 Total Stores"
-# And I should see exactly 10 results (There are 17 stores within 25 miles. We want to display 10 on this page and 7 on the next. Get the first page to work first from start to finish and worry about pagination later.)
-# And I should see the long name, city, distance, phone number and store type for each of the 10 results
   end
 end
