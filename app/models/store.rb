cf0fb925c25397700e58a567bce53057051785c0
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-13 11:05:44 -0600

    minor test for poro passes, creation of arrays confirmed

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,11 +1,27 @@
 class Store
 
+  attr_reader :name,
+              :city,
+              :distance,
+              :phone,
+              :type
+
+  def initialize(info = {})
+    @name     = info[:longName]
+    @city     = info[:city]
+    @distance = info[:distance]
+    @phone    = info[:phone]
+    @type     = info[:type]
+  end
+
 
   def self.find_stores(zip)
     @conn = Faraday.new(url: "https://api.bestbuy.com")
     response = @conn.get("/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,phone,distance&apiKey=#{ENV['bb_key']}")
-    result = JSON.parse(response.body)
-    
+    result = JSON.parse(response.body, symbolize_names: :true)[:stores]
+    result.map do |raw_stores|
+      Store.new(raw_stores)
+    end
   end
 
 end
--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe Store, type: :model do
   it "returns search results in json" do
     VCR.use_cassette("features/user_can_search_for_best_buys_spec.rb") do
 
-      Store.find_stores("80202")
-      
+      result = Store.find_stores("80202")
+
+      expect(result.count).to eq(10)
     end
   end
 end
