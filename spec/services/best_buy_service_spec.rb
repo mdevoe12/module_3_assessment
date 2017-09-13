29ad76f33782415d5ab823674e234671b7b0b641
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-13 12:00:30 -0600

    added additional service tests

--- spec/services/best_buy_service_spec.rb
+++ spec/services/best_buy_service_spec.rb
@@ -6,4 +6,24 @@ RSpec.describe BestBuyService do
 
     expect(conn.class).to eq(Faraday::Connection)
   end
+
+  it "returns the url for finding by zip" do
+    zip = "80211"
+    search_params = "sample"
+    result = BestBuyService.find_zip_url(zip)
+
+    expect(result).to eq("/v1/stores(area(80211,25))?format=json&show=storeType,longName,city,phone,distance&&apiKey=#{ENV['bb_key']}")
+  end
+
+  it "returns the search params" do
+    result = BestBuyService.search_params
+
+    expect(result).to eq("?format=json&show=storeType,longName,city,phone,distance&&apiKey=#{ENV['bb_key']}")
+  end
+
+  it "returns the api key param" do
+    result = BestBuyService.api_key
+
+    expect(result).to eq("&apiKey=#{ENV['bb_key']}")
+  end
 end
