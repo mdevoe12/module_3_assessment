9a6d98ba53eb1bb3668a6ab7a0eb20a332741992
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-13 11:47:47 -0600

    broke url into separate method

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -6,18 +6,13 @@ class BestBuyService
 
   def self.find_by_zip(zip)
     # response = connect.get("/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,phone,distance&apiKey=#{ENV['bb_key']}")
-    response = connect.get("/v1/stores(area(#{zip},25))?#{zip_search_params}&apiKey=#{ENV['bb_key']}")
-
+    response = connect.get(BestBuyService.find_zip_url(zip))
     result = JSON.parse(response.body, symbolize_names: :true)[:stores]
-    binding.pry
-
+    # binding.pry
   end
 
-  def self.zip_search_params
-    {
-      :format => 'json',
-      :show => ['storeType', 'longName', 'city', 'phone', 'distance']
-    }
+  def self.find_zip_url(zip)
+    "/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,phone,distance&apiKey=#{ENV['bb_key']}"
   end
 
 end
