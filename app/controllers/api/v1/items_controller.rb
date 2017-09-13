5e9f83d19700a40fbdd5d13c724b01d8c9e4a3f0
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-13 09:23:06 -0600

    initial test passes for finding single item via api

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::ItemsController < ApplicationController
 
+  def show
+    render json: Item.select("id, name, description, image_url").find(params[:id])
+  end
+
   def index
     render json: Item.select("id, name, description, image_url").all
   end
