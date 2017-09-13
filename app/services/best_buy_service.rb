64c2ad29e81b1f942ed64e5810c5f72c0a8f70ce
Matt DeVoe <matthew.devoe@gmail.com>
2017-09-13 11:42:03 -0600

    trying to pass search params through separate method

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -5,8 +5,19 @@ class BestBuyService
   end
 
   def self.find_by_zip(zip)
-    response = connect.get("/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,phone,distance&apiKey=#{ENV['bb_key']}")
+    # response = connect.get("/v1/stores(area(#{zip},25))?format=json&show=storeType,longName,city,phone,distance&apiKey=#{ENV['bb_key']}")
+    response = connect.get("/v1/stores(area(#{zip},25))?#{zip_search_params}&apiKey=#{ENV['bb_key']}")
+
     result = JSON.parse(response.body, symbolize_names: :true)[:stores]
+    binding.pry
+
+  end
+
+  def self.zip_search_params
+    {
+      :format => 'json',
+      :show => ['storeType', 'longName', 'city', 'phone', 'distance']
+    }
   end
 
 end
